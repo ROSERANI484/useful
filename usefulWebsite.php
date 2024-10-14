263f790daf36af26a481baa5fd4cae39ffabdffe
ROSERANI484 <156175623+ROSERANI484@users.noreply.github.com>
2024-10-14 11:28:56 +0530

    Add files via upload

---
+++ usefulWebsite.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php include 'header.php'?>
+
+  <section class="heading-page header-text" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h6>Get all details</h6>
+          <h2>Online Teaching and Learning Tools</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="meetings-page" id="meetings">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="filters">
+                <ul>
+                  <li data-filter="*"  class="active">All Meetings</li>
+                  <li data-filter=".soon">Soon</li>
+                  <li data-filter=".imp">Important</li>
+                  <li data-filter=".att">Attractive</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="row grid">
+                <div class="col-lg-4 templatemo-item-col all soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful2.png" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>Global Agricultural and Food Submit 2018-for Shaping the <br>Future of Agriculture and Food.</h4>
+                      <!-- Visit Website button placed below the title -->
+                      <a href="http://agrifoodsummit.jharkhand.gov.in/about-us/" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful1.png" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>One of Indias first private sector<br> meteorology and environmental technology companies:<br></h4></a>
+                      <!-- Visit Website button placed below the title -->
+                      <a href="http://weathersysbkc.com/" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful4.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                     <h4>Dr. Sarkar shared a video link <br>from the Agritech Fair at BAU,<br> featuring farmers and agriculture.</h4></a>
+                     <!-- Visit Website button placed below the title -->
+                     <a href="https://youtu.be/ppLOr7fEaMA'" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful7.png" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                     <h4>For Iphone - Fasal Salah App (for mobiles) - Empowering Farmers and Enhancing Agriculture.</h4>
+                     <!-- Visit Website button placed below the title -->
+                     <a href="https://apple.co/2NzwpLa" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful5.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>For Android - Fasal Salah App (for mobiles) - Empowering Farmers and Enhancing Agriculture.</h4>
+                      <!-- Visit Website button placed below the title -->
+                     <a href="https://goo.gl/5Yu1u9" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful0.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>"Global Warming: Understanding the Basics and Addressing Key Management Issues."</h4>
+                         <!-- Visit Website button placed below the title -->
+                     <a href="https://www.nrdc.org/stories/global-warming-101" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all imp att">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful00.png" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>Trainers Training - By Dr. N.P. Singh A video on teaching and training including their per-requisites."</h4>
+                         <!-- Visit Website button placed below the title -->
+                     <a href="https://youtu.be/BEoKbWwl6U8" class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all soon imp">
+                  <div class="meeting-item">
+                    <div class="thumb">>
+                      <a href="meeting-details.html"><img src="assets/images/useful10.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                      <h4>"4 Key Reasons Why You Are Not Getting Job Interviews and Simple Ways to Fix Them."</h4>
+                      <a href='https://jobs.washingtonpost.com/article/4-reasons-you-re-not-getting-interviews-you-may-be-overlooking' class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 templatemo-item-col all att soon">
+                  <div class="meeting-item">
+                    <div class="thumb">
+                      <a href="meeting-details.html"><img src="assets/images/useful11.jpg" alt=""></a>
+                    </div>
+                    <div class="down-content">
+                     <h4>Organic Fertilizers: Benefits, Application Methods, Best Practices, and Tips for Maximum Yield.</h4></a>
+                     <a href='https://www.organicfacts.net/organic-products/organic-cultivation/benefits-of-organic-fertilizers.html' class="btn btn-danger mt-3" target="_blank">Visit Website</a>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="pagination">
+                <ul>
+                  <li><a href="#">1</a></li>
+                  <li class="active"><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+  <section class="contact-us" id="contact">
+  <?php include 'footer.php'?>
+  </section>
+</body>
+</html>
